8a790a686ae7f56ba3bb6d0ad2e97233a5d83eaf
Huynh Quoc Phong <phonghq@dtsmart.vn>
2022-09-30 14:32:03 +0700

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="relative banner">
+        <!-- <img  class="absolute right-0 bottom-1/2 translate-y-1/2 image-1" src="@/assets/images/home/banner-1.png" alt=""> -->
+        <img  class="absolute top-0 left-0 image-2 w-full h-full" src="@/assets/images/home/banner-2.png" alt="">
+    </div>
+</template>
+
+<style lang="scss">
+    .banner{
+        height: 1080px;
+        background-color: #FFF7EB;
+        .image-1{
+            width: 1075px;
+            height: 820px;
+        }
+
+    }
+</style>
